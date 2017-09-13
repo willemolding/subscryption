e3065b9ceebda8ce54f83214300b86dd15eeaaae
Willem Olding <willemolding@gmail.com>
2017-09-13 15:27:10 +1000

    adds service contract and tests

---
+++ contracts/ServiceContract.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.0;
+
+contract ServiceContract {
+
+	bytes32 public serviceName; // name of the service that is being subscribed to
+
+	address owner; // address of the owner of the service that is being paid for. Also the owner of the contract
+	address creator; // who created the contract. They do not have any special privilages
+	address beneficiary; // address of party that gets a percentage of proceeds
+
+	uint public price; // how much the service costs in wei
+	uint beneficiaryShare; // how much goes to the beneficiary in wei per ether
+
+	// a record of how much wei has been paid to each userID
+	mapping(bytes6 => uint) balances;
+
+
+	function ServiceContract(bytes32 serviceName_, address owner_, address beneficiary_, uint price_, uint beneficiaryShare_){
+		// sets the owner to the creator of the contract. It is not necessarily the creator
+		price = price_;
+		serviceName = serviceName_;
+	}
+
+	// fallback called when someones sends funds to this contract. This is not allowed at this time
+	function() payable {
+		revert();
+	}
+
+
+	// view function to find is a user has paid the required amount
+	function isPaid(bytes6 userID) constant returns (bool) {
+		return balances[userID] >= price;
+	}
+
+	// gets the balance of a users account
+	function accountBalance(bytes6 userID) constant returns (uint) {
+		return balances[userID];
+	}
+
+	// method to add ether to a userID. 
+	// The actual ether is passed on to the owner and beneficiary but a record is stored in the contract
+	// no ether should ever be stored in the contract
+	function addEther(bytes6 userID) payable {
+		balances[userID] += msg.value;
+
+		uint beneficiaryCut = msg.value * beneficiaryShare;
+		uint ownerCut = msg.value - beneficiaryCut;
+		
+		assert(beneficiaryCut+ownerCut == msg.value);
+		beneficiary.transfer(beneficiaryCut);
+		owner.transfer(ownerCut);
+	}
+
+
+
+
+
+
+}
---
+++ test/TestServiceContract.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.4.2;
+
+import "truffle/Assert.sol";
+import "../contracts/ServiceContract.sol";
+
+contract TestServiceContract {
+
+	uint public initialBalance = 10 ether;
+
+
+	function testContructor() {
+		bytes32 name = "testService";
+		address owner = msg.sender;
+		address beneficiary = msg.sender;
+		uint price = 1 ether;
+		uint share = 100 finney;
+		ServiceContract sc = new ServiceContract(name, owner, beneficiary, price, share);
+
+		Assert.equal(price, sc.price(), "price should have been set by constructor");
+		Assert.equal(name, sc.serviceName(), "service name should have been set by constructor");
+
+	}
+
+	function testEnablingUserWithExactPayment() {
+		bytes32 name = "testService";
+		address owner = msg.sender;
+		address beneficiary = msg.sender;
+		uint price = 1 ether;
+		uint share = 100 finney;
+		ServiceContract sc = new ServiceContract(name, owner, beneficiary, price, share);
+
+		bytes6 userID = "123456";
+		Assert.equal(sc.isPaid(userID), false, "User has not paid so function should return false");
+		sc.addEther.value(price).gas(30000)(userID);
+		Assert.equal(sc.isPaid(userID), true, "User has paid so function should return true");
+
+	}
+
+	function testEnablingUserWithExcessPayment() {
+		bytes32 name = "testService";
+		address owner = msg.sender;
+		address beneficiary = msg.sender;
+		uint price = 1 ether;
+		uint share = 100 finney;
+		ServiceContract sc = new ServiceContract(name, owner, beneficiary, price, share);
+
+		bytes6 userID = "123456";
+		Assert.equal(sc.isPaid(userID), false, "User has not paid so function should return false");
+		sc.addEther.value(price + 1 wei).gas(30000)(userID);
+		Assert.equal(sc.isPaid(userID), true, "User has paid so function should return true");
+
+	}
+
+	function testEnablingUserWithLessPayment() {
+		bytes32 name = "testService";
+		address owner = msg.sender;
+		address beneficiary = msg.sender;
+		uint price = 1 ether;
+		uint share = 100 finney;
+		ServiceContract sc = new ServiceContract(name, owner, beneficiary, price, share);
+
+		bytes6 userID = "123456";
+		Assert.equal(sc.isPaid(userID), false, "User has not paid so function should return false");
+		sc.addEther.value(price - 1 wei).gas(30000)(userID);
+		Assert.equal(sc.isPaid(userID), false, "User has paid insufficient funds so should return false");
+
+	}
+
+
+
+
+}
