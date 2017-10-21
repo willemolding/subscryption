ec4abcc5a2835daf69b6fefc33efbd60f692f495
willemolding <willemolding@gmail.com>
2017-10-22 09:07:32 +1100

    adds basic subscription abilities to ServiceContract. Untested

--- contracts/ServiceContract.sol
+++ contracts/ServiceContract.sol
@@ -4,22 +4,28 @@ import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract ServiceContract{
 
+    struct AccountData {
+        uint256 totalPaid; // The total value in wei the user has ever paid to the contract
+        uint256 paidUntil; // timestamp to which the user will be valid based on their payments
+    }
+
 	bytes32 public serviceName; // name of the service that is being subscribed to
 
 	address public owner; // address of the owner of the service that is being paid for. Also the owner of the contract
 	address public creator; // who created the contract. They do not have any special privilages
 	address public beneficiary; // address of party that gets a fraction of proceeds
 
-	uint256 public price; // how much the service costs in wei
+	uint256 public price; // how much the service costs in wei per billing period
+	uint256 public billingPeriod; // Duration in seconds that is paid for by price. If 0 then only one-off payments are required
 	uint256 public beneficiaryShare; // how much goes to the beneficiary in wei per ether
 
-	// a record of how much wei has been paid to each userID. This is for full Dapp mode
-	mapping(bytes6 => uint256) private balances;
+	// a mapping of userIDs to their account data
+	mapping(bytes6 => AccountData) private accounts;
 
 	event PaymentReceived(address indexed sender, uint256 value);
 
 
-	function ServiceContract(bytes32 serviceName_, address owner_, address beneficiary_, uint256 price_, uint256 beneficiaryShare_) payable {
+	function ServiceContract(bytes32 serviceName_, address owner_, address beneficiary_, uint256 price_, uint256 billingPeriod_, uint256 beneficiaryShare_) payable {
 		require(msg.value == 0); // do not allow payments to this contract ever
 
 		// sets the owner to the creator of the contract. It is not necessarily the creator
@@ -27,6 +33,7 @@ contract ServiceContract{
 		owner = owner_;
 		beneficiary = beneficiary_;
 		price = price_;
+        billingPeriod = billingPeriod_;
 		beneficiaryShare = beneficiaryShare_;
 		creator = msg.sender;
 
@@ -41,40 +48,52 @@ contract ServiceContract{
 	function() external {
 	}
 
-	// view function to find is a user has paid the required amount
-	function isPaid(bytes6 userID) public constant returns (bool) {
-		return balances[userID] >= price;
+	// view function to find is a user has paid the required amount at the current time
+	function isEnabled(bytes6 userID) public constant returns (bool) {
+		return accounts[userID].paidUntil >= block.timestamp;
 	}
 
-	// gets the balance of a users account
-	function accountBalance(bytes6 userID) public constant returns (uint256) {
-		return balances[userID];
+	// gets the value the user has paid to their account
+	function getTotalPaid(bytes6 userID) public constant returns (uint256) {
+		return accounts[userID].totalPaid;
 	}
+
+    // gets the timestamp to which the user has paid
+    function getPaidUntil(bytes6 userID) public constant returns (uint256) {
+        return accounts[userID].paidUntil;
+    }
 
 	// method to add ether to a userID. 
 	// The actual ether is passed on to the owner and beneficiary but a record is stored in the contract
 	// no ether should ever be stored in the contract 
 	function addEther(bytes6 userID) external payable {
-		if (msg.value > 0){
-			balances[userID] = SafeMath.add(balances[userID], msg.value);
+        require(msg.value > 0);
 
-			uint256 beneficiaryCut = SafeMath.div(SafeMath.mul(msg.value, beneficiaryShare), 1 ether);
-			
-			//transfer the cut to the beneficiary
-			// If this call fails then everything will be rolled back 
-			// no further code execution will occur
-			// The users funds will be recalled (minus gas)
-			beneficiary.transfer(beneficiaryCut);
+        accounts[userID].totalPaid = SafeMath.add(accounts[userID].totalPaid, msg.value);
+        uint256 paidDuration =  SafeMath.mul(msg.value, billingPeriod);
 
-			// transfer everything remaining to the owner. 
-			// If this call fails then the beneficiary won't receive funds and the user won't be authenticated
-			// the users funds will be recalled though (minus gas)
-			// Another call can be used to recover these funds although the address cannot be changed
-			owner.transfer(this.balance);
+        if (isEnabled(userID)) {
+            accounts[userID].paidUntil = SafeMath.add(accounts[userID].paidUntil, paidDuration);
+        } else {
+            accounts[userID].paidUntil = SafeMath.add(block.timestamp, paidDuration);
+        }
 
-			//trigger event
-			PaymentReceived(msg.sender, msg.value);			
-		}
+        uint256 beneficiaryCut = SafeMath.div(SafeMath.mul(msg.value, beneficiaryShare), 1 ether);
+
+        //transfer the cut to the beneficiary
+        // If this call fails then everything will be rolled back
+        // no further code execution will occur
+        // The users funds will be recalled (minus gas)
+        beneficiary.transfer(beneficiaryCut);
+
+        // transfer everything remaining to the owner.
+        // If this call fails then the beneficiary won't receive funds and the user won't be authenticated
+        // the users funds will be recalled though (minus gas)
+        // Another call can be used to recover these funds although the address cannot be changed
+        owner.transfer(this.balance);
+
+        //trigger event
+        PaymentReceived(msg.sender, msg.value);
 
 	}
 
